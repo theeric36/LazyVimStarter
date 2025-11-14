ba500be7e0acd4fb67cd8b9a6fc6d302d2aa540f
Zang Bin <zangbin36@gmail.com>
2025-11-14 15:52:32 +0800

    add p, J, K keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("x", "p", "pgvy", { desc = "Pasting without replacing clipboard" })
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down", silent = true })
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up", silent = true })
