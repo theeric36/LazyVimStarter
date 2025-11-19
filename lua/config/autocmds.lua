3693a5b5b5afe53eb68d3a877c9f883b30147089
Zang Bin <zangbin36@gmail.com>
2025-11-19 16:33:35 +0800

    disable autoformat for javascript and vue

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,7 +7,7 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "python" },
+  pattern = { "python", "javascript", "vue" },
   callback = function()
     vim.b.autoformat = false
   end,
