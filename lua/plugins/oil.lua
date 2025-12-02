5788465e0a3e2a5b2e21fed168b1ae407573e1b9
Zang Bin <zangbin36@gmail.com>
2025-12-02 09:25:19 +0800

    oil float border

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
   ---@type oil.SetupOpts
   opts = {
     default_file_explorer = false,
+    float = {
+      border = "rounded",
+    },
   },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
