a93ccc7c208437f05bfdf7a80f28a9a21c1ca13e
Zang Bin <zangbin36@gmail.com>
2025-12-08 08:50:21 +0800

    oil or snacks.explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,21 +1,31 @@
 return {
-  "stevearc/oil.nvim",
-  enabled = false,
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    default_file_explorer = false,
-    float = {
-      border = "rounded",
+  {
+    "stevearc/oil.nvim",
+    enabled = false,
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      -- default_file_explorer = false,
+      float = {
+        border = "rounded",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      { "<leader>-", "<cmd>Oil --float .<cr>", desc = "Open parent directory" },
+    },
+    use_default_keymaps = false,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      explorer = {
+        -- enabled = false,
+      },
     },
   },
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-  keys = {
-    { "<leader>-", "<cmd>Oil --float .<cr>", desc = "Open parent directory" },
-  },
-  use_default_keymaps = false,
 }
